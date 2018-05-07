31df29bccd0166fa7f34af1c5301e8199400d2d7
Saajan Bariana <sbariana@gmail.com>
2018-05-06 22:24:24 -0700

    Added 4 graphs

--- FinalProject.py
+++ FinalProject.py
@@ -17,6 +17,7 @@ Used to remove "years old" from each data entry for displaying to chart
 """
 def trimText(text):
     update = str(text)[0:str(text).find(" years")].strip()
+    print("Update: " + update)
     newstring = ""
     for i in update:
         if i != " ":
@@ -26,21 +27,61 @@ def trimText(text):
 """
 Used to plot the graphs
 """
-def plotGraph(xAxisValues, yAxisValues, xLabel, yLabel, title):
-    # Plot the keys along with their values
-    plt.plot(xAxisValues, yAxisValues)
+def plotGraph(xAxisValues, yAxisValues, xLabel, yLabel, title, type, explode = None, pctdistance = 0.6, radius = 1, figsize = None):
 
+
+    # Plot the keys along with their values
+    if type == "line":
+        plt.plot(xAxisValues, yAxisValues)
+    elif type == "bar":
+        plt.bar(xAxisValues, yAxisValues)
+    elif type == "pie":
+        fig_size = plt.rcParams["figure.figsize"]
+        fig_size[0] = 10
+        fig_size[1] = 8
+        plt.rcParams["figure.figsize"] = fig_size
+        plt.pie(x=xAxisValues, labels=yAxisValues, autopct='%1.1f%%', explode=explode, pctdistance=pctdistance, radius= radius)
+        if figsize != None:
+            plt.figure(figsize=figsize)
+        # fig = plt.gcf()
+        # fig.set_size_inches(2.5, 5)
+        # fig = plt.figure(figsize=[20, 20])
     # Set the X, Y, and Title labels
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
     plt.title(title)
 
-    # Display the graphe
+
+
+    # Display the graph
     plt.show()
 
+"""
+Used to set the ages in order
+"""
+def orderAges(somedict, filters):
+    # Initialize a dictionary to order the ages
+    updatingdict = {}
 
+    # Go through each age group and receive only the first number
+    for i in somedict:
+        if i not in filters:
+            updatingdict[int(i[0:str(i).find(" ")])] = i
 
+    # Sort all of these numbers and store them into another dictionary with their corresponding values
+    sortedDictionary = {}
+    if len(filters) == 3:
+        sortedDictionary[filters[1]] = somedict[filters[1]]
 
+    for i in sorted(updatingdict.keys()):
+        sortedDictionary[updatingdict[i]] = somedict[updatingdict[i]]
+
+    for j in range(2, len(filters)):
+        temp = filters[j][0:filters[j].find(" ")].strip()
+        temp += "+ " + filters[j][filters[j].find(" "):len(filters[j])]
+        sortedDictionary[temp] = somedict[filters[j]]
+
+    return sortedDictionary
 
 
 
@@ -60,36 +101,60 @@ def readFile():
 """
 Used to plot the Age Information
 """
-def getAge():
+def getAgeTheyStartedCoding():
     #Store the age data into a Series object
     ageSeries = df["q1AgeBeginCoding"]
 
     #Store the unique age entries and the number for each entry into a dictionary
     somedict = dict(ageSeries.value_counts())
 
-    #Initialize a dictionary to order the ages
-    updatingdict = {}
-
-    #Go through each age group and receive only the first number
-    for i in somedict:
-        if i != "#NULL!" and i != "50+ years or older":
-            updatingdict[int(i[0:str(i).find(" ")])] = i
-
-    #Sort all of these numbers and store them into another dictionary with their corresponding values
-    sortedDictionary = {}
-    for i in sorted(updatingdict.keys()):
-        sortedDictionary[updatingdict[i]] = somedict[updatingdict[i]]
-    sortedDictionary["50+ years or older"] = somedict["50+ years or older"]
+    sortedDictionary = orderAges(somedict, ["#NULL!", "50+ years or older"])
 
     #Remove excess text from all the keys in the dictionary
     Keys = []
     for i in sortedDictionary.keys():
         Keys.append( "         " + trimText(i) + "         " )
 
-    plotGraph(Keys, sortedDictionary.values(), "Years Old", "Number of People", "When Did Most Users Start Coding?")
+    plotGraph(Keys, sortedDictionary.values(), "Years Old", "Number of People", "When Did Most Users Start Coding?", "line")
+
+def getCurrentAge():
+    currentAgeSeries = df["q2Age"]
+
+    ageDictionary = dict(currentAgeSeries.value_counts())
+
+    sortedDictionary = orderAges(ageDictionary, ["#NULL!", "Under 12 years old", "75 years or older"])
+
+    print(str(sortedDictionary))
+
+    Keys = []
+    for i in sortedDictionary.keys():
+        print("I: " + i)
+        Keys.append("         " + trimText(i) + "         ")
+    print(str(Keys))
+    plotGraph(Keys, sortedDictionary.values(), "Years Old", "Number of People", "How Old are the Users Now?", "bar")
+
+def getGender():
+    genderSeries = df["q3Gender"]
+
+    genderDictionary = dict(genderSeries.value_counts())
+
+    del genderDictionary["#NULL!"]
+
+    print(str(genderDictionary))
+
+    plotGraph(genderDictionary.values(), genderDictionary.keys(), "", "", "Gender of HackerRank Users", "pie", (0, 0.05, 0.05))
 
 
+def getEducationLevel():
+    educationSeries = df["q4Education"]
 
+    educationDictionary = dict(educationSeries.value_counts())
+
+    del educationDictionary["#NULL!"]
+
+    print(str(educationDictionary))
+
+    plotGraph(educationDictionary.values(), educationDictionary.keys(), "", "", "Education Level of HackerRank Users", "pie", explode=(0.05, 0.05, 0.05, 0.05 ,0.05, 0.15, 0.2), radius=0.7, pctdistance=0.8)
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~INITIAL METHOD~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -97,6 +162,7 @@ def getAge():
 
 if __name__ == "__main__":
     readFile()
-    getAge()
-
-
+    # getAgeTheyStartedCoding()
+    # getCurrentAge()
+    # getGender()
+    getEducationLevel()
